0165190f6685a4cab3763fc2c8299e751d9b1a41
Priyachauhan11 <147380434+Priyachauhan11@users.noreply.github.com>
2026-06-24 07:45:09 +0000

    variable done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "priya@test.com"
+var accountPassword = "1234"
+accountCity = "Mumbai"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail ="pc@.com"
+accountPassword ="2313"
+accountCity ="jaipur"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
