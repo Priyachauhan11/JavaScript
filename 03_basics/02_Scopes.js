7872ba326b24c96a9800216a6ebdb4bf4406f2bf
Priyachauhan11 <147380434+Priyachauhan11@users.noreply.github.com>
2026-06-30 10:59:45 +0000

    scope basics

---
+++ 03_basics/02_Scopes.js
@@ -0,0 +1,19 @@
+// var c = 200
+let a = 200
+if (true){
+    let a = 10
+    const b = 20
+    // var c = 30
+    // c = 40
+    console.log("inner: ", a);
+    
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
